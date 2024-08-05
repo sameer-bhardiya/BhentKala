f666a476ecbbe6aedf0010d867d6651385126c83
sameer <sameerbhardiya@gmail.com>
2024-08-05 20:01:56 +0530

    first commit

---
+++ client/src/components/Announcement.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    height:30px;
+    width:100%;
+    background-color:teal;
+    color:white;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    font-size:14px;
+    font-weight:bold;
+    z-index: 1001;
+    position: fixed;
+    /* top:0; */
+`
+
+const Announcement = () => {
+  return (
+    <Container>
+       All the Art you need in one place
+    </Container>
+  )
+}
+
+export default Announcement
